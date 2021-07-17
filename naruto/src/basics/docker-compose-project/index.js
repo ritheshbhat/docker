b4c8e583b5ab3f7cfe4faa31cadb764dddaee633
Rithesh <rithesh@rithesh-Inspiron-5370>
2021-07-18 02:19:05 +0530

    simple react app for testing docker production grade flow

--- naruto/src/basics/docker-compose-project/index.js
+++ naruto/src/basics/docker-compose-project/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const redis = require('redis');
 
+const process = require('process');
+
+
 const app = express();
 const client = redis.createClient({
     host: 'redis-server' ,//name of service in docker compose,
@@ -11,6 +14,7 @@ client.set('visits', 0);
 
 
 app.get("/", (req,res ) => {
+    // process.exit(0) testing container restart in docker-compose
     client.get('visits', (err,visits)=> {
         res.send("No of visits is " + visits);
         client.set('visits', parseInt(visits) + 1);
---
+++ naruto/src/basics/notes/cmds.md
@@ -0,0 +1,8 @@
+###run docker compose in bg
+
+# docker-compose up --build -d
+-d is for bg
+
+###stop all containers of docker compose at once
+
+#docker-compose down
