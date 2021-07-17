b1793e34701fddc8059c5c84814ae4246b7b2a55
Rithesh <rithesh@rithesh-Inspiron-5370>
2021-07-18 01:33:34 +0530

    simple docker compose project

---
+++ naruto/src/basics/docker-compose-project/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server' ,//name of service in docker compose,
+    port: 6379
+});
+
+client.set('visits', 0);
+
+
+app.get("/", (req,res ) => {
+    client.get('visits', (err,visits)=> {
+        res.send("No of visits is " + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+
+app.listen(8081, () => {
+    console.log("listening on port 4001")
+})
